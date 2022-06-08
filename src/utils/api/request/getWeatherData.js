a3287630a5b38ca0daf2e75e4bdac001a848c4e7
Florian <florian.fort0@gmail.com>
2022-06-08 20:25:16 +0200

    feat: handle error response api

--- src/utils/api/request/getWeatherData.js
+++ src/utils/api/request/getWeatherData.js
@@ -3,9 +3,11 @@ import { reqfetch } from 'utils/api'
 const WEATHER_API = process.env.REACT_APP_WEATHER_API_URL
 const API_KEY = process.env.REACT_APP_API_KEY
 
-console.log(WEATHER_API)
+
 export const getWeatherData = async (city = 'Paris') => {
-   const { data, error } = await reqfetch(`${WEATHER_API}/forecast?q=${city}&appid=${API_KEY}&units=metric`)
+   const { data, error } = await reqfetch(`${WEATHER_API}/forecast?q=${city}&exclude=minute&appid=${API_KEY}&units=metric`)
+
+   if (data?.message === 'city not found' || data?.message === 'Nothing to geocode') return { error: 'error'}
 
    return { data, error }
 }
