c153f9becb242494070ad52e4e38b6d5516851fe
Florian <florian.fort0@gmail.com>
2022-06-08 20:37:56 +0200

    feat: trad icons

---
+++ src/utils/icons.js
@@ -0,0 +1,12 @@
+
+export const ICONS = {
+   'Clear' : 'Clear',
+   'Clouds': 'LightCloud',
+   'scattered clouds': 'HeavyCloud',
+   'broken clouds': 'HeavyCloud',
+   'shower rain':  'HeavyRain',
+   'Rain': 'LightRain',
+   'Thunderstorm': 'Thunderstorm',
+   'Snow': 'Snow',
+   'mist': 'Snow',
+}
